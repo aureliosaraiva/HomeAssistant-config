595f9b5ba1ec5a5d0520b03ceb5fb508e71d1fd7
Aurelio Saraiva <aureliosaraiva@gmail.com>
2021-04-04 15:31:51 -0300

    create macro speech

--- config/custom_components/global_macro/__init__.py
+++ config/custom_components/global_macro/__init__.py
@@ -1,17 +1,42 @@
 import logging
-from homeassistant.helpers.template import Template
+import random
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.template import Template, is_template_string
 
 
 _LOGGER = logging.getLogger(__name__)
 
 
-def global_macro():
-    return "global macro"
+DOMAIN = 'global_macro'
+CONF_SPEECH = 'speech'
 
 
 def setup(hass, config):
-    _LOGGER.info("Setup custom macro")
+    _LOGGER.info("Setup global_macro")
+    speech_category = dict()
+
+    for n in config[DOMAIN]:
+        speech = config[DOMAIN][n]
+        for s in speech:
+            speech_category[s] = speech[s]
+
     hass_tpl = Template("", hass)
-    hass_tpl._env.globals['global_macro'] = global_macro
+    hass_tpl._env.globals['global_macro'] = _global_macro
+    hass_tpl._env.globals['random_speech'] = _speech(speech_category)
 
     return True
+
+
+def _speech(config):
+    def _function(name):
+        try:
+            speechs = config.get(name)
+            return random.choice(speechs)
+        except:
+            return 'Error'
+    return _function
+
+
+def _global_macro():
+    return "global macro"
