e886f146a4fb731087ffb3101f76e048d8e2e135
Aurelio Saraiva <aureliosaraiva@gmail.com>
2021-04-04 16:23:34 -0300

    added macro speaker

--- config/custom_components/global_macro/__init__.py
+++ config/custom_components/global_macro/__init__.py
@@ -15,17 +15,39 @@ CONF_SPEECH = 'speech'
 def setup(hass, config):
     _LOGGER.info("Setup global_macro")
     speech_category = dict()
+    speaker = dict()
+
+    _LOGGER.info(config[DOMAIN])
 
     for n in config[DOMAIN]:
-        speech = config[DOMAIN][n]
-        for s in speech:
-            speech_category[s] = speech[s]
+        if n == 'speech':
+            speech = config[DOMAIN][n]
+            for s in speech:
+                speech_category[s] = speech[s]
+        else:
+            sp = config[DOMAIN][n]
+            for s in sp:
+                speaker[s] = sp[s]
 
+    _LOGGER.info(speaker)
     hass_tpl = Template("", hass)
     hass_tpl._env.globals['global_macro'] = _global_macro
     hass_tpl._env.globals['random_speech'] = _speech(speech_category)
+    hass_tpl._env.globals['speaker'] = _speaker(speaker)
+
 
     return True
+
+def _speaker(speaker):
+    def _function(name):
+        try:
+            if name in speaker:
+                return speaker[name]
+            else:
+                return speaker['default']
+        except:
+            return 'Error'
+    return _function
 
 
 def _speech(config):
